98cd648e2ccb243d81e121520ca216f819166d54
Miguel Ángel <migueabellan@gmail.com>
2019-06-13 13:40:57 +0200

    Update home

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout justify-center align-center>
-      <v-flex shrink>
+  <v-container pa-0 ma-0 class="home">
+    <v-layout row wrap>
+      <v-flex xs12>
+        <Metas
+          :title="title"
+          :description="description"
+          :image="image"
+          :keywords="keywords"
+        />
         <section class="text-xs-center">
           <h1 class="mb-3">
             Asociación Programo Ergo Sum
@@ -16,57 +22,124 @@
           </h2>
         </section>
       </v-flex>
+      <!--
+      <v-layout wrap justify-center class="home-button">
+        <v-flex
+          md2
+          xs6
+          :class="{ 'home-link-item': $vuetify.breakpoint.smAndUp }"
+          class="text-xs-center"
+        >
+          <v-btn
+            outline
+            class="home-link"
+            color="white"
+            href="http://github.com/KP-Kepra"
+            target="_blank"
+          >
+            <v-icon small left class="fab fa-github-square home-icon" />
+            <div class="caption">Github</div>
+          </v-btn>
+        </v-flex>
+        <v-flex
+          md2
+          xs6
+          :class="{ 'home-link-item': $vuetify.breakpoint.smAndUp }"
+          class="text-xs-center"
+        >
+          <v-btn
+            outline
+            class="home-link"
+            color="white"
+            href="https://www.linkedin.com/in/kevin-pratama/"
+            target="_blank"
+          >
+            <v-icon small left class="fab fa-linkedin home-icon" />
+            <div class="caption">LinkedIn</div>
+          </v-btn>
+        </v-flex>
+        <v-flex
+          md2
+          xs6
+          :class="{ 'home-link-item': $vuetify.breakpoint.smAndUp }"
+          class="text-xs-center"
+        >
+          <v-btn
+            outline
+            color="white"
+            class="home-link"
+            href="https://kp-kepra.github.io/static/CV.pdf"
+            target="_blank"
+          >
+            <v-icon small left class="fas fa-pen-square home-icon" />
+            <div class="caption">Resume</div>
+          </v-btn>
+        </v-flex>
+        <v-flex
+          md2
+          xs6
+          :class="{ 'home-link-item': $vuetify.breakpoint.smAndUp }"
+          class="text-xs-center"
+        >
+          <v-btn
+            outline
+            class="home-link"
+            color="white"
+            href="mailto:kevinpr153@gmail.com"
+          >
+            <v-icon small left class="fas fa-envelope-square home-icon" />
+            <div class="caption">Email</div>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+      -->
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import Metas from '@/components/Layout/Metas'
+
 export default {
-  layout: 'index',
-  head() {
-    const title = process.env.title
-    const description = process.env.description
-    const keywords = process.env.keywords
-    const image = process.env.image
-    const url = process.env.canonical + this.$route.path
+  components: {
+    Metas
+  },
+  asyncData() {
     return {
-      title: title,
-      meta: [
-        { rel: 'canonical', href: url },
-        { name: 'description', content: description },
-        { name: 'keywords', content: keywords },
-        { property: 'og:title', content: title },
-        { property: 'og:description', content: description },
-        { property: 'og:image', content: image },
-        { property: 'og:url', content: url },
-        { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:title', content: title },
-        { name: 'twitter:description', content: description },
-        { name: 'twitter:image', content: image }
-      ]
+      title: process.env.title,
+      description: process.env.description,
+      keywords: process.env.keywords,
+      image: process.env.image
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  font-weight: 600;
-  font-size: 40px;
-}
-h2 {
-  max-width: 600px;
-  margin-left: auto;
-  margin-right: auto;
-  font-weight: 300;
-  strong {
-    font-weight: 500;
-    color: #d25f34;
+.home {
+  max-width: 100%;
+  min-height: 100vh;
+  max-height: 100vh;
+  margin-top: -70px !important;
+  padding-top: 35vh !important;
+  h1 {
+    font-weight: 600;
+    font-size: 40px;
   }
-  .stress {
-    padding: 2px 5px;
-    background: #d25f34;
-    color: #fff;
+  h2 {
+    max-width: 600px;
+    margin-left: auto;
+    margin-right: auto;
+    font-weight: 300;
+    strong {
+      font-weight: 500;
+      color: #d25f34;
+    }
+    .stress {
+      padding: 2px 5px;
+      background: #d25f34;
+      color: #fff;
+    }
   }
 }
 </style>
