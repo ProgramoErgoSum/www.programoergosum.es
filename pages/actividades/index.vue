80e7661c137d5ae24c5b87e31c688a3794680102
Miguel Ángel <migueabellan@gmail.com>
2019-06-13 18:47:52 +0200

    Update actividades

--- pages/actividades/index.vue
+++ pages/actividades/index.vue
@@ -7,15 +7,15 @@
         :image="image"
         :keywords="keywords"
       />
-      <v-flex v-for="activity in activities" :key="activity.alias" md12 lg4>
+      <v-flex v-for="activity in activities" :key="activity.alias" xs12 md6 lg4>
         <v-card class="ma-3" :to="`/actividades/${activity.alias}`">
           <v-img
             :src="`/actividades/${activity.alias}/images/preview.png`"
             :title="`${activity.title}`"
           />
           <v-card-title primary-title>
-            <h3 class="mb-2">{{ activity.title }}</h3>
-            <div>{{ activity.description }}</div>
+            <h3 class="mb-3 title">{{ activity.title }}</h3>
+            <div class="subheading">{{ activity.description }}</div>
           </v-card-title>
         </v-card>
       </v-flex>
