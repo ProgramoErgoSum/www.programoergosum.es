912c7e4fe297370ce649faf33d9d32a695cfc152
Miguel Ángel <migueabellan@gmail.com>
2019-06-13 18:38:40 +0200

    Add 404

---
+++ pages/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container fill-height>
+    <v-layout justify-center align-center>
+      <v-flex shrink>
+        <div>
+          <h1 class="display-1 font-weight-medium text-center">
+            Error 404: Página no encontrada
+          </h1>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  head: () => ({
+    title: 'Error 404: Página no encontrada'
+  })
+}
+</script>
+
+<style lang="scss" scope></style>
