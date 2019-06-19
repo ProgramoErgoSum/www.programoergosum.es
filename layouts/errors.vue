5ad038ca542bb9b0deafb27f05b8ff58b02b5562
Miguel Ángel <migueabellan@gmail.com>
2019-06-19 21:12:38 +0200

    Fix errors

--- layouts/errors.vue
+++ layouts/errors.vue
@@ -1,13 +1,41 @@
 <template>
-  <v-app>
-    <v-content>
-      <nuxt class="nuxt-content" />
-    </v-content>
-  </v-app>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex>
+        <div class="text-xs-center">
+          <h1 class="errorcode font-weight-black">
+            {{ error.statusCode }}
+          </h1>
+          <h2 class="display">
+            <p v-if="error.statusCode == 404">
+              Página no encontrada
+            </p>
+            <p v-else>
+              {{ error.message }}
+            </p>
+          </h2>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.errorcode {
+  text-align: center;
+  vertical-align: middle;
+  font-size: 170px;
+  line-height: 170px;
+}
+</style>
