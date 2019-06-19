e2607635ec57062faa7be1200e9fcefacf66ad6f
Miguel Ángel <migueabellan@gmail.com>
2019-06-19 18:59:42 +0200

    Layout 404 error

--- layouts/errors.vue
+++ layouts/errors.vue
@@ -1,13 +1,41 @@
 <template>
   <v-app>
     <v-content>
-      <nuxt class="nuxt-content" />
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex>
+            <div class="text-xs-center">
+              <h1 class="errorcode font-weight-black">
+                {{ error.statusCode }}
+              </h1>
+              <h2 class="display ">Página no encontrada</h2>
+              <NuxtLink to="/">
+                Home page
+              </NuxtLink>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.errorcode {
+  text-align: center;
+  vertical-align: middle;
+  font-size: 170px;
+  line-height: 170px;
+}
+</style>
