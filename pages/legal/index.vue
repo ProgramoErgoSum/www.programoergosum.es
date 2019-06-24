20e616f83d9bd249dbe70cccd316536dc171fd02
Miguel Ángel <migueabellan@gmail.com>
2019-06-24 19:35:02 +0200

    Fix

---
+++ pages/legal/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="legal">
+    <v-container>
+      <v-layout>
+        <v-flex>
+          <!--
+          <Content :readme="readme" />
+          -->
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    const title = 'Información'
+
+    return {
+      title: title,
+      meta: [{ name: 'robots', content: 'nofollow' }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
