6a00b0e29f02754283ec6f3980f828a1ec4f3866
Miguel Ángel <migueabellan@gmail.com>
2019-06-25 20:34:02 +0200

    Remove redirects middelware

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,13 +69,6 @@ export default {
   css: ['~/assets/style/app.styl', '~/assets/style/main.scss'],
 
   /*
-   ** Middleware
-   */
-  router: {
-    middleware: 'redirects'
-  },
-
-  /*
    ** Plugins to load before mounting the App
    */
   plugins: [
