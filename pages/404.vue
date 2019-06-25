d30c553fe218a4c53b03537c201f580fffe5b1a7
Miguel Ángel <migueabellan@gmail.com>
2019-06-25 20:40:40 +0200

    Add 404

---
+++ pages/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="legal">
+    <v-container>
+      <v-layout>
+        <v-flex>
+          404
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    const title = '404'
+
+    return {
+      title: title,
+      meta: [{ name: 'robots', content: 'nofollow' }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
