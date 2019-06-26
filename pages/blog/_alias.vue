7abbe176eb4083261874c8517841f58bbc61a2d6
Miguel Ángel <migueabellan@gmail.com>
2019-06-26 10:54:01 +0200

    error 404 default

--- pages/blog/_alias.vue
+++ pages/blog/_alias.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-// import blogs from '@/static/blog/list.json'
+import blogs from '@/static/blog/list.json'
 /*
 import Metas from '@/components/Layout/Metas'
 import Title from '@/components/Layout/Title'
@@ -33,14 +33,14 @@ export default {
     Title,
     Content
   },
-  
+  */
   validate({ params }) {
     const blog = blogs.find(e => {
       return e.alias === params.alias
     })
     return blog !== undefined
   }
-  
+  /*
   async asyncData({ params, error }) {
     const blog = blogs.find(e => {
       return e.alias === params.alias
