989081c85cc4f55ad16ddbb29274af692f82e67a
Miguel Ángel <migueabellan@gmail.com>
2019-06-26 13:29:50 +0200

    Update

--- pages/blog/_alias.vue
+++ pages/blog/_alias.vue
@@ -58,29 +58,20 @@ export default {
       keywords: blog.keywords,
       readme: readme
     }
-  }
-  /*
-  async asyncData({ params, error }) {
-    const blog = blogs.find(e => {
-      return e.alias === params.alias
-    })
+  },
+  head() {
+    const title = this.title
 
-    const path = `blog/${params.alias}`
-    const file = await import(`@/static/${path}/README.md`)
-    const readme = {
-      path: path,
-      body: file.body,
-      image: `${path}/preview.png`
-    }
     return {
-      title: blog.title,
-      description: blog.description,
-      image: readme.image,
-      keywords: blog.keywords,
-      readme: readme
+      title: title,
+      titleTemplate: 'Blog - %s',
+      meta: [
+        {
+          hid: 'description', name: 'description', content: this.description
+        }
+      ]
     }
   }
-  */
 }
 </script>
 
