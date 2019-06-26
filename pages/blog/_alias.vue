735cc89a1c7d9ab2540aa7ced8e2108e01863947
Miguel Ángel <migueabellan@gmail.com>
2019-06-26 11:22:16 +0200

    error 404 default

--- pages/blog/_alias.vue
+++ pages/blog/_alias.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="_alias">
     {{ title }}
+    <!--
     <Metas
       :title="title"
       :description="description"
       :image="image"
       :keywords="keywords"
     />
-    <!--
     <Title :title="title" :description="description" :image="image" />
     <v-container>
       <v-layout>
@@ -22,41 +22,29 @@
 
 <script>
 import blogs from '@/static/blog/list.json'
-import Metas from '@/components/Layout/Metas'
 /*
+import Metas from '@/components/Layout/Metas'
 import Title from '@/components/Layout/Title'
 import Content from '@/components/Markdown/Content'
 */
 export default {
+  /*
   components: {
-    Metas
-    // Title,
-    // Content
+    Metas,
+    Title,
+    Content
   },
+  */
   validate({ params }) {
     const blog = blogs.find(e => {
       return e.alias === params.alias
     })
     return blog !== undefined
   },
-  async asyncData({ params, error }) {
-    const blog = blogs.find(e => {
-      return e.alias === params.alias
-    })
-
-    const path = `blog/${params.alias}`
-    const file = await import(`@/static/${path}/README.md`)
-    const readme = {
-      path: path,
-      body: file.body,
-      image: `${path}/preview.png`
-    }
+  asyncData({ params, error }) {
+    const title = params.alias
     return {
-      title: blog.title,
-      description: blog.description,
-      image: readme.image,
-      keywords: blog.keywords,
-      readme: readme
+      title: title
     }
   }
   /*
