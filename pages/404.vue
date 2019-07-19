ac1c9a54b33be03a33f82c1c0735b1706d607630
Miguel Ángel <migueabellan@gmail.com>
2019-07-19 17:37:50 +0200

    Refactor 404 page

--- pages/404.vue
+++ pages/404.vue
@@ -1,14 +1,18 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
+  <v-container>
+    <v-layout>
       <v-flex>
-        <div class="text-xs-center">
+        <div class="ma-5 pa-5 text-xs-center">
           <h1 class="errorcode font-weight-black">
             404
           </h1>
           <h2 class="display">
-            <p>
-              Página no encontrada :(
+            <p class="mb-5 pb-5">Página no encontrada <span>&#128534;</span></p>
+            <p class="font-weight-light">
+              La página a la que estás intentando acceder ya no existe. En su
+              lugar puedes acceder a la
+              <nuxt-link to="/">página principal</nuxt-link> o navegar a través
+              del menú de navegación superior o inferior.
             </p>
           </h2>
         </div>
@@ -19,7 +23,6 @@
 
 <script>
 export default {
-  layout: 'empty',
   head() {
     const title = '404 - Página no encontrada'
 
@@ -43,5 +46,9 @@ export default {
   vertical-align: middle;
   font-size: 170px;
   line-height: 170px;
+  color: #d25f34;
+  span {
+    font-size: 30px;
+  }
 }
 </style>
