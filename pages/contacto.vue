01ab9018e0a01ef1f3c4c42c73f3605317f3815b
Miguel Ángel <migueabellan@gmail.com>
2019-07-19 19:09:59 +0200

    Add faqs

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -2,15 +2,30 @@
   <div id="contacto">
     <Title :title="metas.title" :description="metas.description" />
     <v-container>
-      <v-layout>
+      <v-layout row wrap>
         <v-flex>
-          <p>
-            Programo Ergo Sum es una
-            <b>iniciativa sin ánimo de lucro</b> inscrita en el Registro de
-            Asociaciones por el Director General de Participación Ciudadana,
-            Unión Europea y Acción Exterior de la Consejería de Presidencia, con
-            el número de registro 12.731/1ª.
-          </p>
+          <h2 class="mb-3">
+            Resolvemos tus dudas más frecuentes
+          </h2>
+          <v-expansion-panel popout class="mb-5">
+            <v-expansion-panel-content v-for="(item, i) in faqs" :key="i">
+              <template v-slot:header>
+                <div class="subheading">
+                  {{ item.title }}
+                </div>
+              </template>
+              <v-card>
+                <v-card-text class="grey lighten-3 subheading">
+                  {{ item.description }}
+                </v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-flex>
+        <v-flex>
+          <h2 class="mb-3">
+            También puedes contactar con nosotros a través de email
+          </h2>
           <v-list class="subheading">
             <v-list-tile>
               <v-list-tile-avatar>
@@ -40,14 +55,37 @@
         </v-flex>
       </v-layout>
     </v-container>
+    <Reg />
   </div>
 </template>
 
 <script>
 import metas from '@/store/api/v1/metas.json'
 
+import Reg from '@/components/Pages/Home/Reg'
+
 export default {
-  components: {},
+  components: {
+    Reg
+  },
+  data: () => ({
+    faqs: [
+      {
+        title: '¿Qué es Programo Ergo Sum?',
+        description:
+          'Programo Ergo Sum es una iniciativa sin ánimo de lucro cuyo objetivo pretende acercar la programación y la robótica a todas las edades a través de proyectos educativos libres y abiertos.'
+      },
+      {
+        title: '¿Cómo puedo colaborar con la iniciativa?',
+        description:
+          'Puedes colaborar con la iniciativa realizando una donación desinteresada a través de la plataforma de crowdfunding Patreon o a través de PayPal. Con la donación podrás aparecer en el apartado de colaboradores solamente.'
+      },
+      {
+        title: '¿Cómo puedo tener una cuenta premium?',
+        description: 'En estos momentos no es posible crear una cuenta premium.'
+      }
+    ]
+  }),
   asyncData() {
     return {
       metas: metas.contacto
