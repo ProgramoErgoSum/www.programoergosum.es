f2efe1a1ffdfbb9088779ef684be533a16e0d854
Miguel Ángel <migueabellan@gmail.com>
2019-07-28 11:23:21 +0200

    Update contacto

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -3,25 +3,30 @@
     <Title :title="metas.title" :description="metas.description" />
     <v-container>
       <v-layout row wrap>
-        <v-flex>
-          <h2 class="mb-3">
-            Resolvemos tus dudas más frecuentes
-          </h2>
-          <v-expansion-panel popout class="mb-5">
-            <v-expansion-panel-content v-for="(item, i) in faqs" :key="i">
-              <template v-slot:header>
-                <div class="subheading">
-                  {{ item.title }}
-                </div>
-              </template>
-              <v-card>
-                <v-card-text class="grey lighten-3 subheading">
-                  {{ item.description }}
-                </v-card-text>
-              </v-card>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-flex>
+        <template v-for="faq in faqs">
+          <v-flex :key="faq.title" xs12>
+            <h2 class="mb-3">
+              {{ faq.title }}
+            </h2>
+            <v-expansion-panel popout class="mb-5">
+              <v-expansion-panel-content
+                v-for="item in faq.items"
+                :key="item.title"
+              >
+                <template v-slot:header>
+                  <div class="subheading">
+                    {{ item.title }}
+                  </div>
+                </template>
+                <v-card>
+                  <v-card-text class="grey lighten-3 subheading">
+                    {{ item.description }}
+                  </v-card-text>
+                </v-card>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-flex>
+        </template>
         <v-flex>
           <h2 class="mb-3">
             También puedes contactar con nosotros a través de email
@@ -71,18 +76,54 @@ export default {
   data: () => ({
     faqs: [
       {
-        title: '¿Qué es Programo Ergo Sum?',
-        description:
-          'Programo Ergo Sum es una iniciativa sin ánimo de lucro cuyo objetivo pretende acercar la programación y la robótica a todas las edades a través de proyectos educativos libres y abiertos.'
+        title: 'Sobre la asociación ...',
+        items: [
+          {
+            title: '¿Qué es Programo Ergo Sum?',
+            description:
+              'Programo Ergo Sum es una iniciativa sin ánimo de lucro cuyo objetivo pretende acercar la programación y la robótica a todas las edades a través de proyectos educativos libres y abiertos.'
+          },
+          {
+            title: '¿Cómo puedo colaborar con la iniciativa?',
+            description:
+              'Puedes colaborar con la iniciativa realizando una donación desinteresada a través de la plataforma de crowdfunding Patreon o a través de PayPal. Con la donación podrás aparecer en el apartado de colaboradores solamente.'
+          }
+        ]
       },
       {
-        title: '¿Cómo puedo colaborar con la iniciativa?',
-        description:
-          'Puedes colaborar con la iniciativa realizando una donación desinteresada a través de la plataforma de crowdfunding Patreon o a través de PayPal. Con la donación podrás aparecer en el apartado de colaboradores solamente.'
+        title: 'Sobre actividades extraescolares ...',
+        items: [
+          {
+            title: '¿Quién puede participar?',
+            description:
+              'En las actividades extraescolares que organizamos puede participar cualquier niño, niña o jóven sin ningún tipo de discriminación. Al ser actividades gratuitas, el padre, madre o tutor del participante deberá estar presente en todo momento (salvo que se exprese lo contrario) haciéndose responsable de los actos u opiniones del participante.'
+          },
+          {
+            title: '¿Cuándo se realizan?',
+            description:
+              'En el apartado de actividades podrás consultar las fechas de cada una de las actividades extraescolares de programación y robótica que organizamos desde la asociación.'
+          },
+          {
+            title: '¿Cuánto hay que pagar?',
+            description:
+              'Todas las actividades extraescolares que realizamos son gratuitas y se accede mediante orden riguroso de reserva previa. En caso de confirmar una asistencia se deberá acudir o avisar obligatoriamente o se sancionará impidiendo el acceso a futuras actividades.'
+          },
+          {
+            title: '¿Quién organiza las actividades?',
+            description:
+              'Las actividades se organizan a través de la asociación con ayuda de los voluntarios mentores. Si para una actividad no se consiguen voluntarios, se cancelará por falta de mentores voluntarios.'
+          }
+        ]
       },
       {
-        title: '¿Cómo puedo tener una cuenta premium?',
-        description: 'En estos momentos no es posible crear una cuenta premium.'
+        title: 'Sobre plataformas educativas ...',
+        items: [
+          {
+            title: '¿Cómo puedo tener una cuenta premium?',
+            description:
+              'En estos momentos no es posible crear una cuenta premium.'
+          }
+        ]
       }
     ]
   }),
