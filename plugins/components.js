344e02c3b5ee475f764c1558fe5caec457248a02
Miguel Ángel <migueabellan@gmail.com>
2019-09-05 19:16:51 +0200

    Add vuetify-lazy-image

--- plugins/components.js
+++ plugins/components.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import VuetifyLazyImage from 'vuetify-lazy-image'
+
 import Title from '~/components/Global/Title.vue'
 
 Vue.component('Title', Title)
+Vue.component('VuetifyLazyImage', VuetifyLazyImage)
