3a48555f93b76ec22001f8cfb2da1495f9d0a36b
Miguel Ángel <migueabellan@gmail.com>
2019-10-10 12:26:07 +0200

    Add isProd bottomsheet

--- components/Layout/Bottomsheet.vue
+++ components/Layout/Bottomsheet.vue
@@ -1,30 +1,32 @@
 <template>
-  <v-bottom-sheet v-model="sheet" persistent>
-    <v-sheet class="pa-12 text-center" height="300px">
-      <h3 class="mb-6 display-1 font-weight-black">
-        ¿Quieres colaborar?
-      </h3>
-      <p class="max-width mb-6 title font-weight-light">
-        Está en tus manos que este proyecto educativo
-        <b class="font-weight-medium">siga siendo libre y gratuito</b>,
-        <b class="font-weight-medium">realiza una donación</b>
-        totalmente desinteresada y estarás colaborando.
-      </p>
-      <v-btn
-        class="ma-3"
-        color="success"
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7N56RY2QKJJDS&source=url"
-        @click="sheet = false"
-      >
-        Realiza una donación
-      </v-btn>
-      <v-btn class="ma-3" depressed color="error" @click="close">
-        Quizá colabore más tarde
-      </v-btn>
-    </v-sheet>
-  </v-bottom-sheet>
+  <div>
+    <v-bottom-sheet v-if="isProduction" v-model="sheet" persistent>
+      <v-sheet class="pa-12 text-center" height="300px">
+        <h3 class="mb-6 display-1 font-weight-black">
+          ¿Quieres colaborar?
+        </h3>
+        <p class="max-width mb-6 title font-weight-light">
+          Está en tus manos que este proyecto educativo
+          <b class="font-weight-medium">siga siendo libre y gratuito</b>,
+          <b class="font-weight-medium">realiza una donación</b>
+          totalmente desinteresada y estarás colaborando.
+        </p>
+        <v-btn
+          class="ma-3"
+          color="success"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7N56RY2QKJJDS&source=url"
+          @click="sheet = false"
+        >
+          Realiza una donación
+        </v-btn>
+        <v-btn class="ma-3" depressed color="error" @click="close">
+          Quizá colabore más tarde
+        </v-btn>
+      </v-sheet>
+    </v-bottom-sheet>
+  </div>
 </template>
 
 <script>
@@ -33,6 +35,11 @@ export default {
   data() {
     return {
       sheet: false
+    }
+  },
+  computed: {
+    isProduction() {
+      return process.env.NODE_ENV === 'production'
     }
   },
   mounted() {
