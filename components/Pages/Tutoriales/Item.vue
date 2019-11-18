0827ccc918c5fb76dd95bfa08c878bd43aaa9a4e
Miguel Ángel <migueabellan@gmail.com>
2019-11-18 12:14:28 +0100

    Add extra tutorials

--- components/Pages/Tutoriales/Item.vue
+++ components/Pages/Tutoriales/Item.vue
@@ -1,60 +1,60 @@
 <template>
   <v-card :to="`/tutoriales/${tutorial.alias}`" outlined nuxt>
-    <VImageLazy :src="absoluteUrlImage" :title="tutorial.title" :height="180" />
+    <VImageLazy :src="absoluteUrlImage" :title="tutorial.title" :height="220" />
     <v-card-title primary-title>
       <h3 class="mb-4 font-weight-medium body-1">{{ tutorial.title }}</h3>
       <div class="mb-2 body-2">{{ tutorial.description }}</div>
+      <!--
       <div class="my-4">
         <Tags :tags="tags" />
       </div>
+      -->
     </v-card-title>
-    <!--
-    <v-card-text v-if="tutorial.extra" class="pt-6 text-center">
-      <div class="d-inline-flex ma-2">
-        <v-tooltip top>
-          <template v-slot:activator="{ on }">
-            <span v-on="on">
-              <v-icon class="mr-2">mdi-school-outline</v-icon>
-              {{ tutorial.extra.level }}
-            </span>
-          </template>
-          <span>Nivel de dificultad</span>
-        </v-tooltip>
+    <v-card-text v-if="tutorial.extra" class="text-center">
+      <div v-if="tutorial.extra.level">
+        <v-chip class="ma-1" outlined>
+          <v-icon size="16" class="mr-1">
+            mdi-school-outline
+          </v-icon>
+          {{ tutorial.extra.level }}
+        </v-chip>
+        <template v-for="tag in tutorial.tags.level">
+          <v-chip :key="tag" class="ma-1" outlined>
+            <v-icon size="16" class="mr-1">
+              mdi-school-outline
+            </v-icon>
+            {{ tag }}
+          </v-chip>
+        </template>
       </div>
-      <div class="d-inline-flex ma-2">
-        <v-tooltip top>
-          <template v-slot:activator="{ on }">
-            <span v-on="on">
-              <v-icon class="mr-2">mdi-clock-outline</v-icon>
-              {{ tutorial.extra.duration }}
-            </span>
-          </template>
-          <span>Duración aproximada</span>
-        </v-tooltip>
+
+      <div v-if="tutorial.extra.duration" class="d-inline-flex">
+        <v-chip class="ma-1" outlined>
+          <v-icon size="16" class="mr-1">
+            mdi-clock-outline
+          </v-icon>
+          {{ tutorial.extra.duration }}
+        </v-chip>
       </div>
-      <div class="d-inline-flex ma-2">
-        <v-tooltip top>
-          <template v-slot:activator="{ on }">
-            <span v-on="on">
-              <v-icon class="mr-2">mdi-video-outline</v-icon>
-              {{ tutorial.extra.videos }}
-            </span>
-          </template>
-          <span>Vídeos en Youtube</span>
-        </v-tooltip>
+      <div v-if="tutorial.extra.videos" class="d-inline-flex">
+        <v-chip class="ma-1" outlined>
+          <v-icon size="16" class="mr-1">
+            mdi-video-outline
+          </v-icon>
+          {{ tutorial.extra.videos }}
+        </v-chip>
       </div>
     </v-card-text>
-    -->
   </v-card>
 </template>
 
 <script>
-import Tags from '@/components/Pages/Tutoriales/Tags'
+// import Tags from '@/components/Pages/Tutoriales/Tags'
 
 export default {
   name: 'Item',
   components: {
-    Tags
+    // Tags
   },
   props: {
     tutorial: {
@@ -65,7 +65,8 @@ export default {
   computed: {
     absoluteUrlImage() {
       return `${this.$store.state.tutoriales.repo_raw}/${this.tutorial.alias}/${this.tutorial.image}`
-    },
+    }
+    /*
     tags() {
       const tags = this.tutorial.tags
       return tags.technology
@@ -74,6 +75,7 @@ export default {
         .concat(tags.level)
         .concat(tags.others)
     }
+    */
   }
 }
 </script>
