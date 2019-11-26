70e35e7c0bbb2504936d6a5481405f370b6caaea
Miguel Ángel <migueabellan@gmail.com>
2019-11-26 11:48:40 +0100

    Update

--- pages/colabora.vue
+++ pages/colabora.vue
@@ -6,12 +6,6 @@
       :image="metas.image"
     />
     <section class="py-12">
-      <Patrons />
-    </section>
-    <section class="py-12" :class="background">
-      <Donations />
-    </section>
-    <section class="py-12">
       <v-container>
         <v-row>
           <v-col cols="12">
@@ -23,41 +17,33 @@
               estableció como una <b>entidad sin ánimo de lucro</b> para que
               cualquier persona pudiera aprender informática, programación,
               electrónica y robótica educativa <b>gratis</b> a través de
-              videotutoriales y sin moverse de su casa.
-            </p>
-            <p>
-              Hemos trabajado duro durante años para cumplir con los fines y
-              objetivos de la
+              videotutoriales y sin moverse de su casa. Hemos trabajado duro
+              durante años para cumplir con los fines y objetivos de la
               <nuxt-link to="/asociacion" title="Asociación Programo Ergo Sum">
                 asociación
               </nuxt-link>
-              y el camino no ha hecho nada más que empezar. Nuestros proyectos
-              educativos, además de gratuitos, son libres y de código abierto,
-              lo que significa que puedes acceder a todo el contenido de los
-              tutoriales así como al código fuente de los proyectos.
+              y el camino no ha hecho nada más que empezar.
             </p>
             <p>
-              Cada día aumentan las visitas de todos nuestros proyectos
-              educativos y requieren de atención técnica. En estos momentos
-              (septiembre de 2019) acceden una media de 5mil usuarios diarios a
-              nuestras plataformas educativas. Además, en el mundo de la
-              informática, tienes que estar actualizado a la última para no
-              recibir ataques informáticos.
+              Nuestros proyectos educativos, <b>además de gratuitos</b>,
+              <b>son libres y de código abierto</b>, lo que significa que puedes
+              acceder a todo el contenido de los tutoriales así como al código
+              fuente.
             </p>
-            <v-list class="mt-6" color="transparent" disabled>
-              <v-subheader class="pa-0 title">
-                ¿Quieres saber a qué se destinan las donaciones?
-              </v-subheader>
+            <h2 class="mt-12 mb-3">
+              ¿A qué se destinan las donaciones?
+            </h2>
+            <v-list class="mt-6" color="transparent" three-line disabled>
               <v-list-item-group>
-                <v-list-item v-for="(item, i) in faqs" :key="i">
+                <v-list-item v-for="(item, i) in faqs" :key="i" class="mb-3">
                   <v-list-item-icon>
-                    <v-icon v-text="item.icon" />
+                    <v-icon size="40" v-text="item.icon" />
                   </v-list-item-icon>
                   <v-list-item-content>
-                    <v-list-item-title>
+                    <v-list-item-title class="title">
                       {{ item.title }}
                     </v-list-item-title>
-                    <v-list-item-subtitle>
+                    <v-list-item-subtitle class="body-1">
                       {{ item.text }}
                     </v-list-item-subtitle>
                   </v-list-item-content>
@@ -67,6 +53,12 @@
           </v-col>
         </v-row>
       </v-container>
+    </section>
+    <section class="py-12" :class="background">
+      <Patrons />
+    </section>
+    <section class="py-12">
+      <Donations />
     </section>
     <Reg />
   </div>
@@ -89,25 +81,25 @@ export default {
         icon: 'mdi-server-network',
         title: 'Infraestructura',
         text:
-          'Nuestas plataformas educativas reciben una media de 5mil visitas diarias.'
+          'Disponemos de varios servidores para mejorar la calidad de conexión a los estudiantes.'
       },
       {
         icon: 'mdi-hammer',
         title: 'Mantenimiento',
         text:
-          'En la informática tienes que estar actualizado a la última para no recibir ataques.'
+          'Actualizamos constantemente el software informático para minimizar los ataques informáticos.'
       },
       {
         icon: 'mdi-code-tags',
         title: 'Desarrollo',
         text:
-          'Para mejorar la calidad de los proyectos desarrollamos mejoras en los proyectos.'
+          'Desarrollamos mejoras en los proyectos educativos para mejorar la calidad en la enseñanza.'
       },
       {
         icon: 'mdi-slack',
         title: 'Soporte',
         text:
-          'Dedicamos tiempo a resolver dudas de nuestros usuarios a través de varios canales.'
+          'Resolvemos las dudas de nuestros alumnos de forma gratuita a través de varios canales.'
       }
     ]
   }),
