cc690575a1abde8412c1913ab65e92676193ae4e
Miguel Ángel <migueabellan@gmail.com>
2019-12-27 10:25:23 +0100

    Update donation

--- components/Pages/Colabora/Donations.vue
+++ components/Pages/Colabora/Donations.vue
@@ -4,32 +4,16 @@
       <v-col cols="12">
         <h2 class="mb-3">Donaciones</h2>
         <p>
-          Si te gusta lo que hacemos o utilizas algunos de nuestros proyectos
-          educativos y quieres ayudarnos aportando tu granito de arena, puedes
+          Si te gusta lo que hacemos y quieres ayudarnos economicamente, puedes
           realizar una
           <b>donación totalmente desinteresada</b> con el importe que creas
-          necesario. Para agradecértelo, aparecerá tu nombre en esta sección.
+          necesario y para agradecértelo, tu nombre aparecerá en esta sección.
         </p>
       </v-col>
       <v-col cols="12">
         <template v-for="(item, key) in donations">
           <v-chip
-            v-if="item.twitter"
-            :key="key"
-            :href="item.twitter"
-            label
-            outlined
-            class="ma-1"
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-          >
-            <v-avatar class="mr-1">
-              <v-icon size="18" color="#00aced">mdi-twitter</v-icon>
-            </v-avatar>
-            {{ item.name }}
-          </v-chip>
-          <v-chip
-            v-else-if="item.website"
+            v-if="item.website"
             :key="key"
             :href="item.website"
             label
@@ -47,6 +31,9 @@
             {{ item.name }}
           </v-chip>
         </template>
+        <p class="mt-3 mb-0 caption">
+          * Donaciones realizadas entre 2015-{{ new Date().getFullYear() }}.
+        </p>
       </v-col>
       <v-col cols="12">
         <div class="my-6">
@@ -72,48 +59,66 @@ export default {
     donations: [
       // 15
       {
-        name: 'Julio Clemente Cordeiro',
-        website: 'http://www.edu.xunta.gal/centros/iessalvadormadariaga'
+        name: 'Julio C Cordeiro (IES Salvador Madariaga)',
+        website: 'http://www.edu.xunta.gal/centros/iessalvadormadariaga',
+        date: '2019-09-29'
       },
       // 10
       {
-        name: 'Juan Antonio Pérez',
-        twitter: 'https://twitter.com/profetonino'
+        name: 'Jesús Barrera (Aprender con robots)',
+        website: 'https://aprenderconrobots.com',
+        date: '2019-12-27'
       },
       {
-        name: 'Sergio Pérez'
+        name: 'Juan Antonio Pérez (IES Murillo)',
+        website: 'https://sites.google.com/iesmurillo.es/tecnomurillo',
+        date: '2019-10-20'
+      },
+      {
+        name: 'Sergio Pérez',
+        date: '2019-11-12'
       },
       // 5
       {
-        name: 'David Pons'
+        name: 'David Pons',
+        date: '2019-07-11'
       },
       {
-        name: 'Francisco José Marín'
+        name: 'Francisco José Marín',
+        date: '2019-11-25'
       },
       {
-        name: 'Javier Garcia'
+        name: 'Javier Garcia',
+        date: '2019-12-15'
       },
       {
-        name: 'José Antonio Torrecillas'
+        name: 'José Antonio Torrecillas',
+        date: '2019-11-17'
       },
       {
-        name: 'María Bouvier'
+        name: 'María Bouvier',
+        date: '2019-12-05'
       },
       {
-        name: 'Maria Moure'
+        name: 'Maria Moure',
+        date: '2019-10-30'
       },
       {
-        name: 'Nasim Maldonado'
+        name: 'Nasim Maldonado',
+        date: '2019-09-30'
       },
       // 1
       {
-        name: 'Andres Montilla'
+        name: 'Andres Montilla',
+        date: '2019-07-25'
       },
       {
-        name: 'Carlos Hernán'
+        name: 'Carlos Hernán',
+        date: '2019-07-10'
       },
       {
-        name: 'Julieta Brenda'
+        name: 'Julieta Brenda',
+        date: '2019-07-11'
       }
     ]
   })
