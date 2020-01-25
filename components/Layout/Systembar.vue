8e82604701d6de62724ee17c5441310b860520da
Miguel Ángel <migueabellan@gmail.com>
2020-01-25 19:51:09 +0100

    Add Systembar

--- components/Layout/Systembar.vue
+++ components/Layout/Systembar.vue
@@ -1,17 +1,16 @@
 <template>
-  <v-system-bar color="secondary" lights-out dark fixed app>
-    <v-icon>mdi-message</v-icon>
-    <span>
+  <v-system-bar color="secondary" window dark fixed app>
+    <div class="subtitle-1 text-center">
+      <v-icon>mdi-message</v-icon>
       ¿Te gustaría que este proyecto educativo siguiese siendo libre y gratuito?
-      Ayúdanos con una
       <a
         href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7N56RY2QKJJDS&source=url"
         target="_blank"
         rel="noopener noreferrer"
       >
-        donación
+        Ayúdanos realizando una donación
       </a>
-    </span>
+    </div>
   </v-system-bar>
 </template>
 
